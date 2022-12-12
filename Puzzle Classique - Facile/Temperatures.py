9f42a0433690d7b7ee372971f9fd4ade85c6bc8f
Vincent Boettcher <vin.br@runbox.no>
2022-12-12 22:24:37 +0100

    Puzzles & challenges with formatting

--- Puzzle Classique - Facile/Temperatures.py
+++ Puzzle Classique - Facile/Temperatures.py
@@ -11,19 +11,21 @@ result = 0
 for i in input().split():
     # t: a temperature expressed as an integer ranging from -273 to 5526
     t = int(i)
-    if t > 0 : positives.append(t)
-    elif t < 0 : negatives.append(t)
+    if t > 0:
+        positives.append(t)
+    elif t < 0:
+        negatives.append(t)
 
 if positives == [] and negatives == []:
     result = 0
-else :
-    if positives == []: 
+else:
+    if positives == []:
         result = max(negatives)
-    elif negatives == [] : 
+    elif negatives == []:
         result = min(positives)
-    elif min(positives) <= abs(min(negatives)) :
+    elif min(positives) <= abs(min(negatives)):
         result = min(positives)
-    elif min(positives) > abs(min(negatives)) :
+    elif min(positives) > abs(min(negatives)):
         result = max(negatives)
 
 print(result)
